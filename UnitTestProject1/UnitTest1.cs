d51396832da228e98c9b14c906392b3cd173931b
MustafaCilek <cilekmustafa51@gmail.com>
2026-03-24 22:07:32 +0100

    # Task 0 Beginning

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.Inconclusive("TBD");
+            throw new System.NotImplementedException("TBD");
+        }
+    }
+}
