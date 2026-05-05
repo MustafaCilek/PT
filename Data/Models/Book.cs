014d34c5487cfe2119c728448f4b3360bbbb8357
Ismail Eyup Gocmen <ismailegocmen@gmail.com>
2026-05-05 18:00:03 +0200

    Added Book and BookCopy Models.

--- Data/Models/Book.cs
+++ Data/Models/Book.cs
@@ -1,1 +1,10 @@
-﻿
+﻿// Data/Models/Book.cs
+namespace Data.Models
+{
+    public class Book
+    {
+        public string Isbn { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+    }
+}
--- Data/Models/BookCopy.cs
+++ Data/Models/BookCopy.cs
@@ -1,1 +1,10 @@
-﻿
+﻿// Data/Models/BookCopy.cs
+namespace Data.Models
+{
+    public class BookCopy
+    {
+        public string CopyId { get; set; }
+        public Book BookDetails { get; set; } // Reference to the Catalog
+        public bool IsAvailable { get; set; } = true;
+    }
+}
