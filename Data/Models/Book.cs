c5e9c624253398a39dbd2cec4f3d8443c0487752
MustafaCilek <cilekmustafa51@gmail.com>
2026-05-05 17:44:46 +0200

    Fixed some bugs.

--- Data/Models/Book.cs
+++ Data/Models/Book.cs
@@ -1,10 +1,1 @@
-﻿// Data/Models/Book.cs
-namespace Data.Models
-{
-    public class Book
-    {
-        public string Isbn { get; set; }
-        public string Title { get; set; }
-        public string Author { get; set; }
-    }
-}
+﻿
--- Data/Models/BookCopy.cs
+++ Data/Models/BookCopy.cs
@@ -1,10 +1,1 @@
-﻿// Data/Models/BookCopy.cs
-namespace Data.Models
-{
-    public class BookCopy
-    {
-        public string CopyId { get; set; }
-        public Book BookDetails { get; set; } // Reference to the Catalog
-        public bool IsAvailable { get; set; } = true;
-    }
-}
+﻿
