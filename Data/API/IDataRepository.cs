552a4eecdf5e1d363c1bec5194f9d912a8cfc117
Ismail Eyup Gocmen <ismailegocmen@gmail.com>
2026-05-05 17:55:07 +0200

    Added IDataRepository

--- Data/API/IDataRepository.cs
+++ Data/API/IDataRepository.cs
@@ -1,1 +1,26 @@
-﻿
+﻿// Data/API/IDataRepository.cs
+using Data.Models;
+using System.Collections.Generic;
+
+namespace Data.API
+{
+    public interface IDataRepository
+    {
+        // Users
+        void AddReader(Reader reader);
+        Reader GetReader(string id);
+
+        // Catalog
+        void AddBookToCatalog(Book book);
+        Book GetBook(string isbn);
+
+        // Process State
+        void AddBookCopy(BookCopy copy);
+        BookCopy GetBookCopy(string copyId);
+        void UpdateBookCopy(BookCopy copy);
+
+        // Events
+        void RecordEvent(LibraryEvent libEvent);
+        IEnumerable<LibraryEvent> GetAllEvents();
+    }
+}
